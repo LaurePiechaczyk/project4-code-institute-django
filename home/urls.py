c968311617fd34cb14c3f9e262c5373bf6bad929
LaurePiechaczyk <laure.piecha@gmail.com>
2021-10-10 08:47:05 +0000

    cleaning the file py in home

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,12 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.home, name='home'), 
-    path('feminine', views.feminine, name='feminine'), 
-    path('masculine', views.masculine, name='masculine'), 
-    path('go_to_category/<category_id>', views.go_to_category, name='go_to_category'), 
-    path('gender_category', views.gender_category, name='gender_category'), 
+    path('', views.home, name='home'),
+    path('feminine', views.feminine, name='feminine'),
+    path('masculine', views.masculine, name='masculine'),
+    path('go_to_category/<category_id>', views.go_to_category, name='go_to_category'),
     path('go_to_category/<category_id>/feminine', views.categorie_feminine, name='categorie_feminine'),
     path('go_to_category/<category_id>/masculine', views.categorie_masculine, name='categorie_masculine'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic, View
-from .models import Noun, GenderToDisplay, NounCategory
+from .models import Noun, NounCategory
 
 ##############
 
@@ -8,79 +7,82 @@ number_of_displayed_word = 4
 ##############
 # Create your views here.
 
+
 def home(request):
-  categories = NounCategory.objects.all()
-  context = {
-    'theme' : 'Chose a theme',
-    'categories' : categories
-  }
-  return render (request, 'home/index.html', context)
-
-
-def gender_category(request):
-  context = index_with_gender_and_category('Feminine', 'cat1')
-  return render (request, 'home/index.html', context)
+    categories = NounCategory.objects.all()
+    context = {
+        'theme': 'Chose a theme',
+        'categories': categories
+      }
+    return render(request, 'home/index.html', context)
 
 
 def feminine(request):
-  context = index_with_noun_list('Feminin')
-  return render (request, 'home/index.html', context)
+    context = index_with_noun_list('Feminin')
+    return render(request, 'home/index.html', context)
 
 
 def masculine(request):
-  context = index_with_noun_list('Masculin')
-  return render (request, 'home/index.html', context)
+    context = index_with_noun_list('Masculin')
+    return render(request, 'home/index.html', context)
 
 
-def go_to_category(request, category_id): 
-  category = get_object_or_404(NounCategory, id=category_id)
-  context = index_with_gender_and_category('', category)
-  return render(request, 'home/index.html', context)
+def go_to_category(request, category_id):
+    category = get_object_or_404(NounCategory, id=category_id)
+    context = index_with_gender_and_category('', category)
+    return render(request, 'home/index.html', context)
 
 
-def categorie_feminine(request,category_id):
-  category = get_object_or_404(NounCategory, id=category_id)
-  context = index_with_gender_and_category('Feminin', category)
-  return render(request, 'home/index.html', context)
-  
+def categorie_feminine(request, category_id):
+    category = get_object_or_404(NounCategory, id=category_id)
+    context = index_with_gender_and_category('Feminin', category)
+    return render(request, 'home/index.html', context)
 
-def categorie_masculine(request,category_id):
-  category = get_object_or_404(NounCategory, id=category_id)
-  context = index_with_gender_and_category('Masculin', category)
-  return render(request, 'home/index.html', context)
+
+def categorie_masculine(request, category_id):
+    category = get_object_or_404(NounCategory, id=category_id)
+    context = index_with_gender_and_category('Masculin', category)
+    return render(request, 'home/index.html', context)
 
 
 ######################
 def index_with_gender_and_category(chosen_gender, chosen_category):
-  nouns = Noun.objects.filter(categories__noun_category__contains = chosen_category).filter(gender__noun_gender__contains = chosen_gender).order_by('?')[:number_of_displayed_word]
-  
-  categories = NounCategory.objects.all()
-  context = {
-    'nouns' : nouns,
-    'categories' : categories,
-    'theme' : chosen_category,
-  }
-  return context
+    nouns = Noun.objects.filter(
+      categories__noun_category__contains=chosen_category).filter(
+        gender__noun_gender__contains=chosen_gender).order_by(
+          '?')[:number_of_displayed_word]
+
+    categories = NounCategory.objects.all()
+    context = {
+      'nouns': nouns,
+      'categories': categories,
+      'theme': chosen_category,
+    }
+    return context
 
 
 def index_with_category(chosen_category):
-  nouns = Noun.objects.filter(categories__noun_category__contains = chosen_category).order_by('?')[:number_of_displayed_word]
-  categories = NounCategory.objects.all()
-  context = {
-    'nouns' : nouns,
-    'categories' : categories,
-    'theme' : chosen_category, 
-    'theme_id' : chosen_category.id,
-  }
-  return context
+    nouns = Noun.objects.filter(
+      categories__noun_category__contains=chosen_category).order_by(
+        '?')[:number_of_displayed_word]
+    categories = NounCategory.objects.all()
+    context = {
+      'nouns': nouns,
+      'categories': categories,
+      'theme': chosen_category,
+      'theme_id': chosen_category.id,
+    }
+    return context
 
 
 def index_with_noun_list(chosen_gender):
-  nouns = Noun.objects.filter(gender__noun_gender__contains = chosen_gender).order_by('?')[:number_of_displayed_word]
-  categories = NounCategory.objects.all()
-  context = {
-    'nouns' : nouns,
-    'categories' : categories,
-    'theme' : 'Chose a theme',
-  }
-  return context
+    nouns = Noun.objects.filter(
+      gender__noun_gender__contains=chosen_gender).order_by(
+        '?')[:number_of_displayed_word]
+    categories = NounCategory.objects.all()
+    context = {
+      'nouns': nouns,
+      'categories': categories,
+      'theme': 'Chose a theme',
+    }
+    return context
