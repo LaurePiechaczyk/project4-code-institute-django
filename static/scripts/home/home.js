b6f62879d3a13766cf9da5bff82c6bcbd7388c3a
LaurePiechaczyk <laure.piecha@gmail.com>
2021-11-01 14:56:07 +0000

    change color depending on the feminine masculine url

--- static/scripts/home/home.js
+++ static/scripts/home/home.js
@@ -1,5 +1,7 @@
 console.log("fffffffffff")
 
+
+// When english is checked --> display english
 $("#english-check-box").click(function(){
     if($('#english-check-box').is(":checked"))
         $(".words-container .english-noun").css("opacity", "1")
@@ -7,12 +9,31 @@ $("#english-check-box").click(function(){
         $(".words-container .english-noun").css("opacity", "0")
 });
 
+// When german is checked --> display german
 $("#plural-check-box").click(function(){
     if($('#plural-check-box').is(":checked"))
         $(".words-container .plural-noun").css("opacity", "1")
     else 
         $(".words-container .plural-noun").css("opacity", "0")
 });
+
+// Change color on click
+//feminine
+$(document).ready(function() {
+    var pathname = window.location.pathname;
+    if (pathname.indexOf('feminine') > -1) {
+        $('.words-container').addClass('die-color');
+    }
+ });
+ //masculine
+ $(document).ready(function() {
+    var pathname = window.location.pathname;
+    if (pathname.indexOf('masculine') > -1) {
+        $('.words-container').addClass('der-color');
+    }
+ });
+
+
 
 /*
 $("#german-check-box").click(function(){
